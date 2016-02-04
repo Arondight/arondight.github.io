571a465e672c07af3745f80bad76bd57ca70b258
Arondight <shell_way@foxmail.com>
2016-02-04 14:33:43 +0800

    v2016-02-04.3

--- themes/init.sh
+++ themes/init.sh
@@ -4,12 +4,12 @@
 # ==============================================================================
 
 function initTheme {
-  local C_DIR=$(dirname $(readlink -f $0))
-  local BACKGROUND_DIR=$C_DIR/hexo-theme-yelee/source/background
+  [[ -z $THEME_DIR ]] && local THEME_DIR=$(dirname $(readlink -f $0))
+  local BACKGROUND_DIR=${THEME_DIR}/hexo-theme-yelee/source/background
 
   if [[ -d $BACKGROUND_DIR ]]
   then
-    rm -rvf $BACKGROUND_DIR/*
+    rm -rvf ${BACKGROUND_DIR}/*
   fi
 
   return $?
